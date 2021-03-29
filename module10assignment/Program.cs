b773c1568e777a8dfea92dc91773834254847a47
Alex Teerlinck <shadows32426@gmail.com>
2021-03-29 13:56:57 -0500

    created choices 1 and 2 from pre-existing program

--- module10assignment/Program.cs
+++ module10assignment/Program.cs
@@ -12,34 +12,49 @@ namespace module10assignment
         static void Main(string[] args)
         {
             logger.Info("Program started");
+            string choice;
+            do{
+                Console.WriteLine("1) Display all blogs");
+                Console.WriteLine("2) Add Blog.");
+                Console.WriteLine("3) Create Post.");
+                Console.WriteLine("4) Display Posts.");
+                Console.WriteLine("Enter any other key to exit.");
+                choice = Console.ReadLine();
 
-            try
-            {
+                if(choice == "1"){
+                    try
+                    {   
+                        var db = new BloggingContext();
+                        var query = db.Blogs.OrderBy(b => b.Name);
 
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
-                {
-                    Console.WriteLine(item.Name);
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex.Message);
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message);
-            }
+                if(choice == "2"){
+                    Console.Write("Enter a name for a new Blog: ");
+                    try{
+                        var name = Console.ReadLine();
 
+                        var blog = new Blog { Name = name };
+
+                        var db = new BloggingContext();
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex.Message);
+                    }
+                }
+            }while(choice != "1"||choice != "2"||choice != "3"||choice != "4");
             logger.Info("Program ended");
         }
     }
